af2b745a8955e74026b484f9d4c2f04050d31a4d
DaniloLima122 <limadanilo06@gmail.com>
2020-08-04 04:05:30 -0300

    Enviando pasta com as páginas do front-end

---
+++ web/src/pages/TeacherForm/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import PageHeader from '../../components/PageHeader'
+
+export default function TeacherForm() {
+    return (
+        <h1>
+            <PageHeader title="Que incrível que você quer dar aulas."/>
+        </h1>
+    )
+}
