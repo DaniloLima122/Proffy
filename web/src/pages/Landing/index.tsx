d872943581582cc536715e5dcb23baf21defd07f
DaniloLima122 <limadanilo06@gmail.com>
2020-08-06 12:25:46 -0300

    Conectando a landing page com o backend!

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { Link } from "react-router-dom";
 
@@ -13,9 +13,23 @@ import giveClassesIcon from "../../assets/images/icons/give-classes.svg";
 import purpleHeartIcon from "../../assets/images/icons/purple-heart.svg";
 
 import "./styles.css";
+import api from '../../services/api';
 
 
 export default function Landing() {
+
+    const [totalConnections, setTotalConnections] = useState(0);
+
+
+    useEffect(() => {
+
+        api.get("connections").then(({ data: { total } }) => {
+
+            setTotalConnections(total)
+
+        });
+    }, [])
+
     return (
         <div id="page-landing">
             <div id="page-landing-content" className="container">
@@ -37,7 +51,7 @@ export default function Landing() {
                         Estudar
                     </Link>
 
-                            <Link to="/give-classes" className="give-classes">
+                    <Link to="/give-classes" className="give-classes">
                         <img src={giveClassesIcon} alt="Dar aulas" />
                         Dar aulas
                     </Link>
@@ -45,7 +59,7 @@ export default function Landing() {
 
 
                 <span className="total-connections">
-                    Total de 200 conexões já realizadas <img src={purpleHeartIcon} alt="Coração roxo" />
+                    Total de {totalConnections} conexões já realizadas <img src={purpleHeartIcon} alt="Coração roxo" />
                 </span>
             </div>
         </div>
