9d48037b59978ad5e0c8c44081950d7f0040ee9e
DaniloLima122 <limadanilo06@gmail.com>
2020-08-07 08:47:33 -0300

    Finalizado layout o header compartilhado

---
+++ mobile/src/components/PageHeader/index.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { View, Image, Text } from 'react-native';
+import { BorderlessButton } from "react-native-gesture-handler";
+
+import { useNavigation } from '@react-navigation/native';
+
+import styles from './styles';
+
+import backIcon from "../../assets/images/icons/back.png";
+import logoImg from "../../assets/images/logo.png";
+
+interface PageheaderProps {
+    title: string
+}
+
+const PageHeader: React.FC<PageheaderProps> = ({ title }) => {
+
+
+    const navigation = useNavigation();
+
+    function goBacktoLanding() {
+
+        navigation.navigate("Landing");
+    }
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.topBar}>
+                <BorderlessButton onPress={goBacktoLanding}>
+                    <Image source={backIcon} resizeMode="contain" />
+                </BorderlessButton>
+
+                <Image source={logoImg} resizeMode="contain" />
+            </View>
+
+            <Text style={styles.title}>{title}</Text>
+
+        </View>
+    )
+}
+
+export default PageHeader;
