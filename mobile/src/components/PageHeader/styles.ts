38914a0959bde2e241a4ea0dd3f88ec9e7cd3e6f
DaniloLima122 <limadanilo06@gmail.com>
2020-08-13 17:37:51 -0300

    Concluidos estilo do Header

--- mobile/src/components/PageHeader/styles.ts
+++ mobile/src/components/PageHeader/styles.ts
@@ -11,7 +11,7 @@ const styles = StyleSheet.create({
         flexDirection:'row',
         alignItems:'center',
         justifyContent:"space-between",
-        marginTop: 5
+        marginTop: 5,
     },
 
     header: {
